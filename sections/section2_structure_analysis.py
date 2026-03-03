70bb60871cc2531656dcc0c48af50a4459a39dcf
lukehtt <dwnz4334@leeds.ac.uk>
2026-03-03 19:28:01 +0000

    1 and 2

---
+++ sections/section2_structure_analysis.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def parity_sequence(n):
+    seq=[]
+    while n!=1:
+        seq.append(n%2)
+        if n%2==0:
+            n//=2
+        else:
+            n=3*n+1
+    seq.append(0)
+    return seq
+
+N=50  
+maxlen=50
+data=np.zeros((N,maxlen))
+for i in range(1,N+1):
+    p=parity_sequence(i)
+    data[i-1,:len(p)]=p[:maxlen]
+
+plt.imshow(data,cmap='binary',interpolation='none')
+plt.xlabel("Step")
+plt.ylabel("Starting number")
+plt.show()
